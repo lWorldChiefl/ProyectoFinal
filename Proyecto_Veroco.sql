0f83b6b0696fa198ca1bb6ae46a5004f7e742cff
Cristian Rodriguez <isc.rodriguezcristian@gmail.com>
2016-12-03 22:59:46 -0800

    Cambios
    Cambios

--- Proyecto_Veroco.sql
+++ Proyecto_Veroco.sql
@@ -6,7 +6,16 @@ Use Proyecto_Veroco
 Create Table Usuarios
 (
 	userId int primary key identity,
+	userName varchar(80),
 	userEmail varchar(120),
 	userPassword varchar(16),
 	userType int
+);
+
+	/*Creamos la Tabla TiposUsuarios*/
+
+Create Table Tipo_Usuarios
+(
+	userTypeId int primary key identity,
+	userTypeDescription varchar(100)
 );
