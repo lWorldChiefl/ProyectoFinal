706ad7aac237b59186acfab6f88921f9c8c7965c
Cristian Rodriguez <isc.rodriguezcristian@gmail.com>
2016-12-03 22:55:00 -0800

    Cambio en el archivo SQL
    Es necesario el cambio

--- Proyecto_Veroco.sql
+++ Proyecto_Veroco.sql
@@ -5,5 +5,8 @@ Use Proyecto_Veroco
 	/*Creamos la Tabla Usuarios*/
 Create Table Usuarios
 (
-	
+	userId int primary key identity,
+	userEmail varchar(120),
+	userPassword varchar(16),
+	userType int
 );
