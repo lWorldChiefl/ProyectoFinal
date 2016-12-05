86da6aa0675d2762ba71b6be3a426731cce340d5
Cristian Rodriguez <isc.rodriguezcristian@gmail.com>
2016-12-05 00:40:43 -0800

    Proyecto Estable V2.4

--- Proyecto_Final/Proyecto_Final/Controllers/VerocoController.cs
+++ Proyecto_Final/Proyecto_Final/Controllers/VerocoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLayer;
+using BusinessLayer;
 
 namespace Proyecto_Final.Controllers
 {
@@ -20,6 +22,16 @@ namespace Proyecto_Final.Controllers
 
         public ActionResult Registrarse()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrarse(Usuario user)
+        {
+            UsuariosRepository userRe = new UsuariosRepository();
+            userRe.Crear(user);
+            ModelState.Clear();
+            ViewBag.Message = user.userName + " registrado satisfactoriamente";
             return View();
         }
     }
