358469e2670fb999ba2fe623fd15ed6141190dfc
Cristian Rodriguez <isc.rodriguezcristian@gmail.com>
2016-12-04 19:49:27 -0800

    Cambios al proyecto

---
+++ Proyecto_Final/BusinessLayer/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class IEntity
+    {
+        public string Id;
+    }
+
+    public interface IRepository<T> where T : IEntity
+    {
+        IEnumerable<T> List { get; }
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T FindById(int Id);
+    }
+}
