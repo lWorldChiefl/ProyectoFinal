ea03bf9ca7ec36afb3f0a7ad8b886e3b381b9cb9
Cristian Rodriguez <isc.rodriguezcristian@gmail.com>
2016-12-05 16:02:58 -0800

    Proyecto Estable Registrar

--- Proyecto_Final/Proyecto_Final/App_Start/RouteConfig.cs
+++ Proyecto_Final/Proyecto_Final/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Proyecto_Final
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Veroco", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Veroco", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
